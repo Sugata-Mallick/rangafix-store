a8cab8f77ab2da3d263056c8d6fee200386323e3
Sugata-Mallick <sugatamallick75@gmail.com>
2021-09-13 23:55:01 +0600

    f

--- js/app.js
+++ js/app.js
@@ -14,30 +14,36 @@ const showProducts = (products) => {
     const { rate, count } = product.rating;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
+    div.innerHTML = `<div style="background-color:cadetblue; margin:20px ;" class="single-product">
+      <div >
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+
+      <h3>Total rating:${count}</h3>
+      <p>Average rating:${rate}</p>
+        
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      
+      
+      <button id="details-btn" class="btn btn-danger" >Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
-const showDetails = (price, rating) => {
-  console.log(price, rating);
-  document.getElementById("modal-body").innerHTML = `
-  <div class="p-3">
-  <p>Rating:${Array.from(Array(parseInt(Rating)).keys()).map(
-    (r) => '<i class="bi bi-star-fill text-warning"></i>'
-  )}</p>
-  <h2>price: $ ${price}</h2>
-  </div>
-`;
-};
+// const showDetails = (price, rating) => {
+//   console.log(price, rating);
+//   document.getElementById("modal-body").innerHTML = `
+//   <div class="p-3">
+//   <p>Rating:${Array.from(Array(parseInt(rating)).keys()).map(
+//     (r) => '<i class="bi bi-star-fill text-warning"></i>'
+//   )}</p>
+//   <h2>price: $ ${price}</h2>
+//   </div>
+// `;
+// };
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
